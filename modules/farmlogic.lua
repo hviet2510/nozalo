7a34558e7544388c0b3daee3b5b30fdb1858643a
hviet2510 <vieth1393@gmail.com>
2025-07-13 01:17:06 +0700

    Update farmlogic.lua

--- modules/farmlogic.lua
+++ modules/farmlogic.lua
@@ -1,20 +1,24 @@
-local module = {}
+local FarmLogic = {}
 
-function module.GetEnemy(player, enemyList)
-    local level = player:FindFirstChild("leaderstats") and player.leaderstats:FindFirstChild("Level")
-    if not level then return nil end
+function FarmLogic.GetEnemy(player, enemyList)
+    local leaderstats = player:FindFirstChild("leaderstats")
+    local level = leaderstats and leaderstats:FindFirstChild("Level")
+    if not level then
+        warn("[FarmLogic] ❌ Không tìm thấy Level!")
+        return nil
+    end
+    local lv = level.Value
 
-    for _, e in ipairs(enemyList) do
-        if level.Value >= e.MinLevel and level.Value <= e.MaxLevel then
+    for _, enemy in pairs(enemyList) do
+        if lv >= enemy.MinLevel and lv <= enemy.MaxLevel then
             for _, mob in pairs(workspace.Enemies:GetChildren()) do
-                if mob.Name == e.Name and mob:FindFirstChild("Humanoid") and mob.Humanoid.Health > 0 then
+                if mob.Name == enemy.Name and mob:FindFirstChild("Humanoid") and mob.Humanoid.Health > 0 then
                     return mob
                 end
             end
         end
     end
-
     return nil
 end
 
-return module
+return FarmLogic
