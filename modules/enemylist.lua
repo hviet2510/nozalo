50b03c6639f3d020202333a7706a7bcb6ebeb6c1
hviet2510 <vieth1393@gmail.com>
2025-07-12 17:08:05 +0700

    Create enemylist.lua

---
+++ modules/enemylist.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        Name = "Bandit",
+        LevelRequired = 0,
+        LevelMax = 9,
+    },
+    {
+        Name = "Monkey",
+        LevelRequired = 10,
+        LevelMax = 14,
+    },
+    {
+        Name = "Gorilla",
+        LevelRequired = 15,
+        LevelMax = 29,
+    },
+    {
+        Name = "Pirate",
+        LevelRequired = 30,
+        LevelMax = 39,
+    },
+    {
+        Name = "Brute",
+        LevelRequired = 40,
+        LevelMax = 59,
+    },
+    {
+        Name = "Desert Bandit",
+        LevelRequired = 60,
+        LevelMax = 74,
+    },
+    -- 👉 Bạn có thể thêm đầy đủ danh sách Sea 1 ở đây
+}
