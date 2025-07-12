2f1434c696e4c87b50109320064c7595f2923f8e
hviet2510 <vieth1393@gmail.com>
2025-07-13 02:11:14 +0700

    Update enemylist.lua

--- modules/enemylist.lua
+++ modules/enemylist.lua
@@ -1,11 +1,10 @@
--- enemylist.lua
 return {
-    "Bandit",
-    "Monkey",
-    "Gorilla",
-    "Brute",
-    "Pirate",
-    "Buggy",
-    "Desert Bandit",
-    "Desert Officer"
+    { Name = "Bandit", MinLevel = 1, MaxLevel = 15 },
+    { Name = "Monkey", MinLevel = 16, MaxLevel = 29 },
+    { Name = "Gorilla", MinLevel = 30, MaxLevel = 54 },
+    { Name = "Pirate", MinLevel = 55, MaxLevel = 69 },
+    { Name = "Brute", MinLevel = 70, MaxLevel = 89 },
+    { Name = "Buggy", MinLevel = 90, MaxLevel = 99 },
+    { Name = "Desert Bandit", MinLevel = 100, MaxLevel = 109 },
+    { Name = "Desert Officer", MinLevel = 110, MaxLevel = 129 }
 }
