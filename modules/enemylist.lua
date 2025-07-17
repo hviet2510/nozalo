5c4eea69ec9c44178d6966d4acfe8f180dae7773
hviet2510 <vieth1393@gmail.com>
2025-07-17 17:19:33 +0700

    Update enemylist.lua

--- modules/enemylist.lua
+++ modules/enemylist.lua
@@ -1,24 +1,218 @@
 return {
-    { Name = "Bandit", MinLevel = 1, MaxLevel = 14 },
-    { Name = "Monkey", MinLevel = 15, MaxLevel = 29 },
-    { Name = "Gorilla", MinLevel = 30, MaxLevel = 54 },
-    { Name = "Pirate", MinLevel = 55, MaxLevel = 69 },
-    { Name = "Brute", MinLevel = 70, MaxLevel = 89 },
-    { Name = "Desert Bandit", MinLevel = 90, MaxLevel = 109 },
-    { Name = "Desert Officer", MinLevel = 110, MaxLevel = 134 },
-    { Name = "Snow Bandit", MinLevel = 135, MaxLevel = 159 },
-    { Name = "Snowman", MinLevel = 160, MaxLevel = 189 },
-    { Name = "Chief Petty Officer", MinLevel = 190, MaxLevel = 209 },
-    { Name = "Sky Bandit", MinLevel = 210, MaxLevel = 249 },
-    { Name = "Dark Master", MinLevel = 250, MaxLevel = 299 },
-    { Name = "Toga Warrior", MinLevel = 300, MaxLevel = 374 },
-    { Name = "Gladiator", MinLevel = 375, MaxLevel = 399 },
-    { Name = "Military Soldier", MinLevel = 400, MaxLevel = 449 },
-    { Name = "Military Spy", MinLevel = 450, MaxLevel = 499 },
-    { Name = "Fishman Warrior", MinLevel = 500, MaxLevel = 549 },
-    { Name = "Fishman Commando", MinLevel = 550, MaxLevel = 624 },
-    { Name = "Warden", MinLevel = 625, MaxLevel = 649 },
-    { Name = "Chief Warden", MinLevel = 650, MaxLevel = 674 },
-    { Name = "Swan", MinLevel = 675, MaxLevel = 699 },
-    { Name = "Magma Marine", MinLevel = 700, MaxLevel = 724 }
+    {
+        Name = "Bandit",
+        MinLevel = 1,
+        MaxLevel = 14,
+        MonsterName = "Bandit",
+        QuestName = "BanditQuest1",
+        QuestLevel = 1,
+        QuestNPCName = "BanditQuestGiver"
+    },
+    {
+        Name = "Monkey",
+        MinLevel = 15,
+        MaxLevel = 29,
+        MonsterName = "Monkey",
+        QuestName = "JungleQuest",
+        QuestLevel = 1,
+        QuestNPCName = "JungleQuestGiver"
+    },
+    {
+        Name = "Gorilla",
+        MinLevel = 30,
+        MaxLevel = 59,
+        MonsterName = "Gorilla",
+        QuestName = "JungleQuest",
+        QuestLevel = 2,
+        QuestNPCName = "JungleQuestGiver"
+    },
+    {
+        Name = "Pirate",
+        MinLevel = 60,
+        MaxLevel = 74,
+        MonsterName = "Pirate",
+        QuestName = "BuggyQuest1",
+        QuestLevel = 1,
+        QuestNPCName = "BuggyQuestGiver"
+    },
+    {
+        Name = "Brute",
+        MinLevel = 75,
+        MaxLevel = 89,
+        MonsterName = "Brute",
+        QuestName = "BuggyQuest1",
+        QuestLevel = 2,
+        QuestNPCName = "BuggyQuestGiver"
+    },
+    {
+        Name = "Desert Bandit",
+        MinLevel = 90,
+        MaxLevel = 104,
+        MonsterName = "Desert Bandit",
+        QuestName = "DesertQuest",
+        QuestLevel = 1,
+        QuestNPCName = "DesertQuestGiver"
+    },
+    {
+        Name = "Desert Officer",
+        MinLevel = 105,
+        MaxLevel = 119,
+        MonsterName = "Desert Officer",
+        QuestName = "DesertQuest",
+        QuestLevel = 2,
+        QuestNPCName = "DesertQuestGiver"
+    },
+    {
+        Name = "Snow Bandit",
+        MinLevel = 120,
+        MaxLevel = 134,
+        MonsterName = "Snow Bandit",
+        QuestName = "SnowQuest",
+        QuestLevel = 1,
+        QuestNPCName = "SnowQuestGiver"
+    },
+    {
+        Name = "Snowman",
+        MinLevel = 135,
+        MaxLevel = 149,
+        MonsterName = "Snowman",
+        QuestName = "SnowQuest",
+        QuestLevel = 2,
+        QuestNPCName = "SnowQuestGiver"
+    },
+    {
+        Name = "Chief Petty Officer",
+        MinLevel = 150,
+        MaxLevel = 174,
+        MonsterName = "Chief Petty Officer",
+        QuestName = "MarineQuest2",
+        QuestLevel = 1,
+        QuestNPCName = "MarineQuestGiver2"
+    },
+    {
+        Name = "Sky Bandit",
+        MinLevel = 175,
+        MaxLevel = 189,
+        MonsterName = "Sky Bandit",
+        QuestName = "SkyQuest",
+        QuestLevel = 1,
+        QuestNPCName = "SkyQuestGiver"
+    },
+    {
+        Name = "Dark Master",
+        MinLevel = 190,
+        MaxLevel = 224,
+        MonsterName = "Dark Master",
+        QuestName = "SkyQuest",
+        QuestLevel = 2,
+        QuestNPCName = "SkyQuestGiver"
+    },
+    {
+        Name = "Toga Warrior",
+        MinLevel = 225,
+        MaxLevel = 274,
+        MonsterName = "Toga Warrior",
+        QuestName = "ColosseumQuest",
+        QuestLevel = 1,
+        QuestNPCName = "ColosseumQuestGiver"
+    },
+    {
+        Name = "Gladiator",
+        MinLevel = 275,
+        MaxLevel = 299,
+        MonsterName = "Gladiator",
+        QuestName = "ColosseumQuest",
+        QuestLevel = 2,
+        QuestNPCName = "ColosseumQuestGiver"
+    },
+    {
+        Name = "Military Soldier",
+        MinLevel = 300,
+        MaxLevel = 324,
+        MonsterName = "Military Soldier",
+        QuestName = "MagmaQuest",
+        QuestLevel = 1,
+        QuestNPCName = "MagmaQuestGiver"
+    },
+    {
+        Name = "Military Spy",
+        MinLevel = 325,
+        MaxLevel = 374,
+        MonsterName = "Military Spy",
+        QuestName = "MagmaQuest",
+        QuestLevel = 2,
+        QuestNPCName = "MagmaQuestGiver"
+    },
+    {
+        Name = "Fishman Warrior",
+        MinLevel = 375,
+        MaxLevel = 399,
+        MonsterName = "Fishman Warrior",
+        QuestName = "FishmanQuest",
+        QuestLevel = 1,
+        QuestNPCName = "FishmanQuestGiver"
+    },
+    {
+        Name = "Fishman Commando",
+        MinLevel = 400,
+        MaxLevel = 449,
+        MonsterName = "Fishman Commando",
+        QuestName = "FishmanQuest",
+        QuestLevel = 2,
+        QuestNPCName = "FishmanQuestGiver"
+    },
+    {
+        Name = "God's Guard",
+        MinLevel = 450,
+        MaxLevel = 474,
+        MonsterName = "God's Guard",
+        QuestName = "SkyExp1Quest",
+        QuestLevel = 1,
+        QuestNPCName = "SkyExp1QuestGiver"
+    },
+    {
+        Name = "Shanda",
+        MinLevel = 475,
+        MaxLevel = 524,
+        MonsterName = "Shanda",
+        QuestName = "SkyExp1Quest",
+        QuestLevel = 2,
+        QuestNPCName = "SkyExp1QuestGiver"
+    },
+    {
+        Name = "Royal Squad",
+        MinLevel = 525,
+        MaxLevel = 549,
+        MonsterName = "Royal Squad",
+        QuestName = "SkyExp2Quest",
+        QuestLevel = 1,
+        QuestNPCName = "SkyExp2QuestGiver"
+    },
+    {
+        Name = "Royal Soldier",
+        MinLevel = 550,
+        MaxLevel = 624,
+        MonsterName = "Royal Soldier",
+        QuestName = "SkyExp2Quest",
+        QuestLevel = 2,
+        QuestNPCName = "SkyExp2QuestGiver"
+    },
+    {
+        Name = "Galley Pirate",
+        MinLevel = 625,
+        MaxLevel = 649,
+        MonsterName = "Galley Pirate",
+        QuestName = "FountainQuest",
+        QuestLevel = 1,
+        QuestNPCName = "FountainQuestGiver"
+    },
+    {
+        Name = "Galley Captain",
+        MinLevel = 650,
+        MaxLevel = 700,
+        MonsterName = "Galley Captain",
+        QuestName = "FountainQuest",
+        QuestLevel = 2,
+        QuestNPCName = "FountainQuestGiver"
+    }
 }
